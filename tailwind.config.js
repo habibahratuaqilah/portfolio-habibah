eca3167b4dcfaabb9eb78df79cc6bc8755e01f38
nada savana <hi@his-MacBook-Air.local>
2024-12-26 14:42:39 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#E195AB",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
